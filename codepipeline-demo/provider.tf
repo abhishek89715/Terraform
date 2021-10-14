f6698eee55171b2d9ec53a0ca3f8ca9861da550c
abhishek <abhishek.89715@gmail.com>
2021-10-14 01:10:14 -0700

    new

---
+++ codepipeline-demo/provider.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
+  region     = var.AWS_REGION
+}
+  data "aws_availability_zones" "available" {
+} 
+
+  data "aws_caller_identity" "current" {
+}
+
